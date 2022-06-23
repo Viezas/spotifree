c756fe78d284680c2ce00b32570f4bb1008d31a6
Viezas <dxviezas@gmail.com>
2022-06-23 09:06:57 +0200

    First commit

---
+++ src/config/aws-exports.js
@@ -0,0 +1,7 @@
+export default {
+    aws_project_region: 'us-east-1',
+    aws_cognito_region: 'us-east-1',
+    aws_user_pools_id: 'us-east-1_Xhb5tHpty',
+    aws_user_pools_web_client_id: '5lqgpkjhfgu49gic9apf06hj5p',
+    oauth: {}
+}
